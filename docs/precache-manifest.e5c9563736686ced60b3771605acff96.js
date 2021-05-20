b33e0dfee1c1e33cb1565ae5b16ba30ccedd5946
Martin Cosentino <martincosentino95@gmail.com>
2021-05-20 15:18:10 -0300

    reuploaded

---
+++ docs/precache-manifest.e5c9563736686ced60b3771605acff96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcef1a4eb7a5e95f350bf3741c85c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5eb9c90257aa54ed29",
+    "url": "/static/js/2.57a411c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57a411c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a72b186a38b8699064",
+    "url": "/static/js/main.af1c4d61.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
